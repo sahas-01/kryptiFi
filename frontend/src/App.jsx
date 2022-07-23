347d03fc84ee7790dcd1647ad9181eafe79a1ab7
Swarup Kharul <swarupkharulsk@gmail.com>
2022-07-23 20:21:27 +0530

    feat!: creator login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,26 @@
 import React, { Component } from "react";
-import { Switch, Route , Redirect, Link} from 'react-router-dom';
-
+import { Switch, Route, Redirect, Link } from "react-router-dom";
 
 // Styling
 import "./App.css";
 import creatorprofile from "./components/creatorprofile/creatorprofile";
 import home from "./components/home/home";
-import Login from "./components/Login/Login";
+import Login from "./components/login/login";
+import CreatorLogin from "./components/login/creatorLogin";
 import Sidebar from "./components/creatorprofile/sections/Sidebar/Sidebar";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="">
-        <Switch>
-          <Route exact path="/" component={Login} />
-        <Route exact path="/home" component={home} />
-        <Route path="/creator" component={creatorprofile} />
-      </Switch>
-      </div>
-      
-    );
-  }
+	render() {
+		return (
+			<div className="">
+				<Switch>
+					<Route exact path="/" component={Login} />
+					<Route exact path="/login" component={CreatorLogin} />
+					<Route exact path="/home" component={home} />
+					<Route path="/creator" component={creatorprofile} />
+				</Switch>
+			</div>
+		);
+	}
 }
 export default App;
