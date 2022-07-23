f0aad87f454a3114d7c53514f869816b2c9fa047
pranavvdesai <pranavdesai.psd@gmail.com>
2022-07-23 23:05:19 +0530

    feat: list creators

--- frontend/src/components/Creatorcard.jsx
+++ frontend/src/components/Creatorcard.jsx
@@ -2,14 +2,15 @@ import React from 'react'
 
 
 const Creatorcard = (props) => {
-  const { image, title, description } = props.blog;
+  // const { image, title, description } = props.blog;
+  const { image, name, description } = props.nft;
   return (
     <div className="w-full ">
       <div className="my-4 md:mx-4 shadow p-6 rounded-md bg-white group hover:shadow-md">
         <div className="relative mb-6 w-full h-56 bg-purple-200 rounded-md overflow-hidden">
           <img
             src={image}
-            alt="blogImage"
+            alt="creatorImage"
             className="w-full h-full object-cover object-center transform group-hover:scale-125 group-hover:rotate-6 transition duration-200"
           />
         </div>
@@ -18,7 +19,7 @@ const Creatorcard = (props) => {
             href="#0"
             className="block text-lg font-medium text-gray-800 hover:text-purple-600 mb-2"
           >
-            {title}
+            {name}
           </a>
         </h3>
         <p className="text-gray-400">{description}</p>
--- frontend/src/components/home/home.jsx
+++ frontend/src/components/home/home.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import image1 from './../../images/blog/blog-01.jpg'
 import image2 from './../../images/blog/blog-01.jpg'
 import image3 from './../../images/blog/blog-01.jpg'
 import image4 from './../../images/blog/blog-01.jpg'
 import UNavbar from '../UNavbar'
 import Creatorcard from '../Creatorcard'
+import { ethers } from 'ethers'
+import axios from 'axios'
+import Web3Modal from 'web3modal'
+import {
+  creatorAddress
+} from '../../config'
+import CreatorNFT from '../../blockchain/artifacts/contracts/CreatorNFT.sol/CreatorNFT.json'
 const blogData = [
   {
     id: 1,
@@ -38,15 +45,46 @@ const blogData = [
 
 
 
-function home() {
+function home () {
+    const [nfts, setNfts] = useState([])
+useEffect(() => {
+    loadNFTs()
+}, [])
+  async function loadNFTs() {
+    /* create a generic provider and query for unsold market items */
+    const provider = new ethers.providers.JsonRpcProvider()
+    const contract = new ethers.Contract(creatorAddress, CreatorNFT.abi, provider)
+    console.log(contract)
+    const data = await contract.fetchMarketItems()
+    /*
+    *  map over items returned from smart contract and format 
+    *  them as well as fetch their token metadata
+    */
+    const items = await Promise.all(data.map(async i => {
+      const tokenUri = await contract.tokenURI(i.tokenId)
+      const meta = await axios.get(tokenUri)
+      let price = ethers.utils.formatUnits(i.price.toString(), 'ether')
+      let item = {
+        price,
+        tokenId: i.tokenId.toNumber(),
+        seller: i.seller,
+        owner: i.owner,
+        image: meta.data.image,
+        name: meta.data.name,
+        description: meta.data.description,
+      }
+      return item
+    }))
+    setNfts(items)
+  }
   return (
     <div>
       <UNavbar />
       <div>
         <section className="pb-10">
       <div className="grid grid-cols-3 md:px-4">
-        {blogData.map((blog, id) => (
-          <Creatorcard blog={blog} key={id} />
+        {nfts.map((nft, i) => (
+          <Creatorcard blog={blog} nft={nft} key={i} />
         ))}
       </div>
     </section>
