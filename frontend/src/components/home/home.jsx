4aa4486c53c8a8e139f3cfee0044c1e1cd65958b
pranavvdesai <pranavdesai.psd@gmail.com>
2022-07-24 01:34:45 +0530

    fix: packages

--- frontend/src/components/home/home.jsx
+++ frontend/src/components/home/home.jsx
@@ -10,36 +10,6 @@ import axios from "axios";
 import Web3Modal from "web3modal";
 import { creatorAddress } from "../../config";
 import CreatorNFT from "../../blockchain/artifacts/contracts/CreatorNFT.sol/CreatorNFT.json";
-const blogData = [
-	{
-		id: 1,
-		image: image1,
-		title: "Name",
-		description:
-			"Prototyping Tool Prototyping is a process that enables faster creativity and effective team.",
-	},
-	{
-		id: 2,
-		image: image2,
-		title: "Name",
-		description:
-			"Wanna decorate your blog with photos? But don't have any photographic skills or fees to pay for photos?",
-	},
-	{
-		id: 3,
-		image: image3,
-		title: "Name",
-		description:
-			"A landing page is a page designed to turn visitors into leads. It is separate from other pages on your...",
-	},
-	{
-		id: 4,
-		image: image4,
-		title: "Name",
-		description:
-			"There is no doubt about the importance of Scalable Vector Graphics illustration today.",
-	},
-];
 
 function home() {
 	const [nfts, setNfts] = useState([]);
@@ -47,7 +17,6 @@ function home() {
 		loadNFTs();
 	}, []);
 	async function loadNFTs() {
-		/* create a generic provider and query for unsold market items */
 		const provider = new ethers.providers.JsonRpcProvider();
 		const contract = new ethers.Contract(
 			creatorAddress,
@@ -56,10 +25,7 @@ function home() {
 		);
 		console.log(contract);
 		const data = await contract.fetchMarketItems();
-		/*
-		 *  map over items returned from smart contract and format
-		 *  them as well as fetch their token metadata
-		 */
+		
 		const items = await Promise.all(
 			data.map(async (i) => {
 				const tokenUri = await contract.tokenURI(i.tokenId);
