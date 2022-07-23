eba833f7a8a75806e032c0c97337b11472bfc024
Sahas <sahasvivek001@gmail.com>
2022-07-24 01:07:02 +0530

    refactor: frontend structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,14 +9,16 @@ import Login from "./components/login/login";
 import CreatorLogin from "./components/login/creatorLogin";
 import Sidebar from "./components/creatorprofile/sections/Sidebar/Sidebar";
 import SignupForm from "./components/login/creatorLoginForm";
+import Common from "./Common";
 
 class App extends Component {
 	render() {
 		return (
 			<div className="">
 				<Switch>
+					<Route path="/common" component={Common} />
 					<Route exact path="/" component={Login} />
-					<Route exact path="/login" component={CreatorLogin} />
+					<Route exact path="/creatorsignup" component={CreatorLogin} />
 					<Route exact path="/signupform" component={SignupForm} />
 					<Route exact path="/home" component={home} />
 					<Route path="/creator" component={creatorprofile} />
---
+++ frontend/src/Common.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function Common() {
+    return (
+        <>
+            <div className="flex items-center justify-center h-screen">
+                <Link to='/'>
+                    <button className="bg-blue-500 hover:bg-blue-700 text-white mx-5 font-bold py-2 px-4 rounded">
+                        User
+                    </button>
+                </Link>
+                <Link to='/signupform'>
+                    <button className="bg-blue-500 hover:bg-blue-700 text-white mx-5 font-bold py-2 px-4 rounded">
+                        Creator
+                    </button>
+                </Link>
+            </div>
+        </>
+    )
+}
+
+export default Common
